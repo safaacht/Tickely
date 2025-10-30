7da6ab6670fc6f338cedd49aae3ffdfca63282a1
safaacht <safaachtaoui@gmail.com>
2025-10-30 23:21:40 +0100

    modife du stepper+ ajout du section2

--- tickely.js
+++ tickely.js
@@ -1,22 +1,12 @@
-const circles=document.querySelectorAll(".circle"),
-progress=document.querySelector(".indicator"),
-nextBtn=document.querySelector("#next");
-
-let currentStep=1;
 
 
-nextBtn.addEventListener("click", () =>{
-    currentStep++;
-    if (currentStep > circles.length) {
-    currentStep = circles.length;
-  }
-
-  updateSteps();
-})
+function handleClick() {
+    console.log("function activated");
+    const carte = document.getElementById("event-container");
+    console.log(carte);
+    carte.style.border = "2px solid blue";
+}
 
 
-currentStep=e.target.id==="next"
-console.log(currentStep);
 
-// click event listner to button
-buttons.addEventListener("click",updateSteps)
+
